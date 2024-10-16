83cc99420d048e1346d0c0f855b5d9343a8f9789
anxielray <anxielworld@gmail.com>
2024-10-16 18:49:00 +0300

    [Add]: Add more functionality to the file info file

--- pkg/fileinfo/fileinfo.go
+++ pkg/fileinfo/fileinfo.go
@@ -1,7 +1,9 @@
-package pkg
+package fileinfo
 
 import (
 	"os"
+	"path/filepath"
+	"syscall"
 	"time"
 )
 
@@ -18,4 +20,31 @@ type FileInfo struct {
 	LinkTarget string
 	Rdev       uint64
 	Blocks     int64
+}
+
+func CreateFileInfo(path string, info os.FileInfo) FileInfo {
+	fileInfo := FileInfo{
+		Name:    info.Name(),
+		Size:    info.Size(),
+		Mode:    info.Mode(),
+		ModTime: info.ModTime(),
+		IsDir:   info.IsDir(),
+		IsLink:  info.Mode()&os.ModeSymlink != 0,
+	}
+
+	if fileInfo.IsLink {
+		linkTarget, err := os.Readlink(filepath.Join(path, info.Name()))
+		if err == nil {
+			fileInfo.LinkTarget = linkTarget
+		}
+	}
+
+	if stat, ok := info.Sys().(*syscall.Stat_t); ok {
+		fileInfo.Nlink = stat.Nlink
+		fileInfo.Uid = stat.Uid
+		fileInfo.Gid = stat.Gid
+		fileInfo.Rdev = stat.Rdev
+	}
+
+	return fileInfo
 }
