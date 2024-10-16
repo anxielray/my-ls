6f7a5466ca7e0c5d96eb8a2457de637dfc123a2e
anxielray <anxielworld@gmail.com>
2024-10-16 17:34:31 +0300

    [Refactor]: Seperate the code into a different file for handling the color reset

---
+++ utils/color-reset.go
@@ -0,0 +1,58 @@
+package utils
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	ColorReset = "\033[0m"
+)
+
+var colorMap map[string]string
+
+func InitColorMap() {
+	colorMap = make(map[string]string)
+	lsColors := os.Getenv("LS_COLORS")
+	if lsColors == "" {
+		return
+	}
+
+	pairs := strings.Split(lsColors, ":")
+	for _, pair := range pairs {
+		parts := strings.Split(pair, "=")
+		if len(parts) == 2 {
+			colorMap[parts[0]] = "\033[" + parts[1] + "m"
+		}
+	}
+}
+
+func Colorize(file FileInfo, name string) string {
+	var colorCode string
+
+	if file.IsDir {
+		colorCode = colorMap["di"]
+	} else if file.IsLink {
+		colorCode = colorMap["ln"]
+	} else if file.Mode&0o111 != 0 {
+		colorCode = colorMap["ex"]
+	} else if file.Mode&os.ModeNamedPipe != 0 {
+		colorCode = colorMap["pi"]
+	} else if file.Mode&os.ModeSocket != 0 {
+		colorCode = colorMap["so"]
+	} else if file.Mode&os.ModeDevice != 0 {
+		colorCode = colorMap["bd"]
+	} else {
+		ext := filepath.Ext(name)
+		if ext != "" {
+			colorCode = colorMap["*"+ext]
+		}
+	}
+
+	if colorCode == "" {
+		return name
+	}
+
+	return colorCode + name + ColorReset
+}
