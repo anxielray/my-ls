e887237ab149943c5d04deea2b4a5cb7c7cc9b33
anxielray <anxielworld@gmil.com>
2024-10-24 06:36:09 +0300

    [update]: Update the utility file code to inlude the modifications when the total should  be displayed

--- pkg/utils/printfiles.go
+++ pkg/utils/printfiles.go
@@ -14,7 +14,7 @@ import (
 )
 
 func PrintLongFormat(files []FI.FileInfo, options OP.Options) {
-	if len(os.Args) == 3 {
+	if len(os.Args) > 2 {
 		//check for the path to display size
 		for _, arg := range os.Args[1:] {
 			if strings.HasPrefix(arg, "-") {
@@ -41,6 +41,7 @@ func PrintLongFormat(files []FI.FileInfo, options OP.Options) {
 	maxMajorWidth := 0
 	maxMinorWidth := 0
 
+	//printing the number of hardlinks of a specific file.
 	for _, file := range files {
 		nlinkWidth := len(fmt.Sprintf("%d", file.Nlink))
 		if nlinkWidth > maxNlinkWidth {
